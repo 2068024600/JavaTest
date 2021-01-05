ddce41fdef005afcf0fefa8cefd5af1582a1cc59
zwh <2068024600@qq.com>
2021-01-05 16:24:13 +0800

    my first JavaTest

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    static String name;
+    public static void main(String[] args) {
+        HelloWorld me = new HelloWorld();
+        me.name = new Scanner(System.in).nextLine();
+        System.out.println(me.name);
+        System.out.println("Hello World"); // 输出 Hello World
+    }
+}
+
